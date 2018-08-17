d2b0513212cd4d84388829d4504c8db6cd2d5325
saumyaagnihotri <s.cnb@rediffmail.com>
2018-08-17 12:19:36 +0530

    codeforces 4A

---
+++ Watermelon4A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin>>num;
+	int c = num - 2;
+	if(num==2)
+	cout<<"NO";
+	else{
+	if(c%2==0)cout<<"YES";
+	else cout<<"NO";
+}
+}
